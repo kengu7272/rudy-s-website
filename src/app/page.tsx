025b7b5220f52a4ba643cdca703f9e344cf5f355
kengu7272 <kengu72@gmail.com>
2024-05-12 02:06:55 -0400

    feat: setup next & tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex bg-red-300 min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-7xl font-semibold">Rudy's Website</h1> 
+    </main>
+  );
+}
